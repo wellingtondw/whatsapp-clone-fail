abf6ea3ac8baedbba4f729f54d99c53590d538be
wellington <wellingtonslpbi@hotmail.com>
2021-03-30 15:46:25 -0300

    handle open/close events

--- src/controller/whatsAppController.js
+++ src/controller/whatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
     
     this.elementsPrototype()
     this.loadElements()
+    this.initEvents()
   }
 
   loadElements() {
@@ -64,4 +65,35 @@ class WhatsAppController {
     }
   }
 
+  initEvents() {
+    this.el.myPhoto.on('click', e => {
+      this.closeAllLeftPanel()
+      this.el.panelEditProfile.show()
+      setTimeout(() => {
+        this.el.panelEditProfile.addClass('open')
+      }, 300)
+
+    })
+
+    this.el.btnNewContact.on('click', e => {
+      this.closeAllLeftPanel()
+      this.el.panelAddContact.show()
+      setTimeout(() => {
+        this.el.panelAddContact.addClass('open')
+      }, 300)
+    })
+
+    this.el.btnClosePanelEditProfile.on('click', e => {
+      this.el.panelEditProfile.removeClass('open')
+    })
+
+    this.el.btnClosePanelAddContact.on('click', e => {
+      this.el.panelAddContact.removeClass('open')
+    })
+  }
+
+  closeAllLeftPanel() {
+    this.el.panelAddContact.hide()
+    this.el.panelEditProfile.hide()
+  }
 }
